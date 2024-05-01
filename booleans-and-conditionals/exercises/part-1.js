d7dd52a5bff21ba6d9844b22ee98d4df11394ab5
Ashley Dougherty <ashleydougherty@Ashleys-MacBook-Air.local>
2024-05-01 18:29:03 -0500

    finished exercises final

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,10 +1,10 @@
 // Declare and initialize the variables for exercise 1 here:
-//let engineIndicatorLight = "red blinking";
-//let spaceSuitsOn = true;
-//let shuttleCabinReady = true;
-//let crewStatus = spaceSuitsOn && shuttleCabinReady;
-//let computerStatusCode = 200;
-//let shuttleSpeed = 15000;
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 // engines are off
@@ -13,27 +13,27 @@
 // Stable speed.
 
 
-//if (engineIndicatorLight === "green") {
-  // console.log("engines have started");
-//} else if (engineIndicatorLight === "green blinking") {
-  // console.log("engines are preparing to start");
-//} else {
-  // console.log("engines are off");
-//}
+if (engineIndicatorLight === "green") {
+   console.log("engines have started");
+} else if (engineIndicatorLight === "green blinking") {
+   console.log("engines are preparing to start");
+} else {
+   console.log("engines are off");
+}
    
-//let fuelLevel = 18000;
-//let engineTemperature = 2500;
+let fuelLevel = 18000;
+let engineTemperature = 2500;
 
-//if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking"){
-  // console.log("ENGINE FAILURE IMMINENT!");
-//} else if (fuelLevel <= 5000 || engineTemperature > 2500){
-  // console.log("Check fuel level. Engines running hot.");
-//} else if (fuelLevel > 20000 && engineTemperature <= 2500){
-  // console.log("Full tank. Engines good.");
-//} else if (fuelLevel > 10000 && engineTemperature <= 2500){
-  // console.log("Fuel level above 50%. Engines good.");
-//} else if (fuelLevel > 5000 && engineTemperature <= 2500){
-  // console.log("Fuel level above 25%. Engines good.");
-//} else {
-  // console.log("Fuel and engine status pending...");
-//}
+if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking"){
+   console.log("ENGINE FAILURE IMMINENT!");
+} else if (fuelLevel <= 5000 || engineTemperature > 2500){
+   console.log("Check fuel level. Engines running hot.");
+} else if (fuelLevel > 20000 && engineTemperature <= 2500){
+   console.log("Full tank. Engines good.");
+} else if (fuelLevel > 10000 && engineTemperature <= 2500){
+   console.log("Fuel level above 50%. Engines good.");
+} else if (fuelLevel > 5000 && engineTemperature <= 2500){
+   console.log("Fuel level above 25%. Engines good.");
+} else {
+   console.log("Fuel and engine status pending...");
+}
